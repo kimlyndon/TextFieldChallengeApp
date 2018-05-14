8735a47f45e9f0b65623849ad2b9ec74030b372a
Kim Lyndon <39sunnyskies@gmail.com>
2018-05-14 18:52:47 -0400

    upload complete delegate

--- TextFieldChallengeApp/TextFieldsDelegates/ZipCodeTextFieldDelegate.swift
+++ TextFieldChallengeApp/TextFieldsDelegates/ZipCodeTextFieldDelegate.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import UIKit
+
+//Define Class
+class ZipCodeTextFieldDelegate: NSObject, UITextFieldDelegate {
+    
+//Text Field Delegate Method
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        return newText.length <= 5
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return true;
+    }
+}
