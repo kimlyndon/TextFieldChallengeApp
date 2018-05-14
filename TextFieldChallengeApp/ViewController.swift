ce46f65376ed2692bf64145103cdf28e1cb1ea82
Kim Lyndon <39sunnyskies@gmail.com>
2018-05-14 18:51:49 -0400

    upload correct VC

--- TextFieldChallengeApp/ViewController.swift
+++ TextFieldChallengeApp/ViewController.swift
@@ -5,53 +5,54 @@
 //  Created by Kim Lyndon on 5/1/18.
 //  Copyright © 2018 Kim Lyndon. All rights reserved.
 //
-
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
+//Properties
+    let zipCodeDelegate = ZipCodeTextFieldDelegate()
+    let cashDelegate = CashTextFieldDelegate()
+    let randomColorDelegate = RandomColorTextFieldDelegate()
     
 //Create outlets for each text field
     @IBOutlet weak var textField1: UITextField!
     @IBOutlet weak var textField2: UITextField!
     @IBOutlet weak var textField3: UITextField!
     @IBOutlet weak var characterCountLabel: UILabel!
+    @IBOutlet weak var editSwitch: UISwitch!
     
-//Text field delegate objects
-    let zipDelegate = ZipCodeTextFieldDelegate()
-    let cashDelegate = CashTextFieldDelegate()
-    let randomColorTextFieldDelegate = RandomColorTextFieldDelegate()
     
 //Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
        
-//Set the label to be hidden
-        self.characterCountLabel.isHidden = true
         
 // Set the three delegates
-    self.textField1.delegate = zipDelegate
-    self.textField2.delegate = cashDelegate
-    self.textField3.delegate =  randomColorTextFieldDelegate
+    self.textField1.delegate = self.zipCodeDelegate
+    self.textField2.delegate = self.cashDelegate
+    self.textField3.delegate = self.randomColorDelegate
+//Set the edit switch
+    self.editSwitch.setOn(false, animated: false)
+    
 }
    
-//Text Field Delegate Methods
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return self.editSwitch.isOn
+    }
     
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-//What will the new text be if we return true
-        var newText = textField.text! as NSString
-        newText = newText.replacingCharacters(in: range, with: string) as NSString
-        
-//Hide the label if the newText is empty string
-        self.characterCountLabel.isHidden = (newText.length == 0)
-        
-//Write the length of newText into the label
-        self.characterCountLabel.text = String(newText.length)
-        
-//Returning true gives the text field permission to change its text
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
         return true;
+    }
+    
+//Edit switch action and toggle.
+   
+    @IBAction func toggleTheTextEditor(_ sender: Any) {
+        if !(sender as! UISwitch).isOn {
+            self.textField3.resignFirstResponder()
+        }
+       
+    }
+    }
+    
 
-
-}
-
-}
